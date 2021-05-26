769b71c363ea10f882da83f2d7ea666ba7680a1c
Mailen-Givre <mailen.givre@gmail.com>
2021-05-25 21:54:34 -0300

    Orden y cambio de nombre de variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 /* Juego de los números - Bulls and Cows - Buco */
 
 
-//TODO salir del juego
-//TODO niveles
-//TODO preguntar info del usuario
+//TODO boton me rindo
+//TODO html niveles
+//TODO html puntajes
 
 
 /* Objeto usuario */
@@ -14,7 +14,6 @@ class usuario {
         this.fecha = fecha;
         this.tiempo = tiempo;
     }
-    
     mostrarJugador () {
         console.log('nombre '+ this.nombre);
         console.log('intentos '+ this.intentos);
@@ -26,22 +25,23 @@ class usuario {
 let usuariosTotales = []
 getUsuarios()
 
-
-// Niveles
+/* Niveles */
 let nivel = 4
 getNivel()
 
 /* La computadora genera un número de 4 cifras que no se repiten */
 
-let cantidadNumeros = nivel // Cantidad de numeros a adivinar. Puede variar.
+let cantidadNumeros = nivel // Cantidad de numeros a adivinar. Varia segun el nivel elegido en el hrml niveles
 let arrayNumerosPc = new Array(cantidadNumeros); //Crear array del numero de la PC
 let numeroPC = 0
 let cantidadDeIntentos = 0
 
 /* Pedirle numeros al usuario */
 let arrayNumerosUsuario = new Array(cantidadNumeros); // Crear array del numero del usuario
-crearNumberGuess() //Crear inputs para que el usuario escriba sus numeros
+crearAdivinarNumero() //Crear inputs para que el usuario escriba sus numeros
 siguienteInput() //Pasa al siguiente input al estar lleno
+borrarInputAnterior() //Backspace borra input o va al anterior
+enterAdivinar() //Enter = boton adivinar
 $("#adivinar").click(adivinar); //Cuando se hace click en el boton adivinar // EVENTO CON JQUERY 
 $("#reiniciar").click(resetear); //Se resetea al hacer click en reiniciar
 
@@ -55,28 +55,49 @@ let errorNoNumero = ""
 let buenas
 let regulares
     
-
 resetear()
 
+/* Curiosidades "API" */
+const URLGET = "../json/curiosidades.json"
+$.get(URLGET, function (respuesta, estado) { 
+    if(estado === "success"){
+      let random =Math.floor(Math.random()*respuesta.length);
+      $("#sabias").html(respuesta[random].curiosidad);
+    }
+})
 
 /* FUNCIONES */
 
+/* Niveles */
+function getNivel(){
+    let nivelString = localStorage.getItem('nivel') //  getItem string
+    if (nivelString != null){
+        nivel= JSON.parse(nivelString)
+    } 
+}
+
+/* Arranca de 0 */
 function resetear(){
     borrarJuego ()
     generarNumeros()
     iniciarIntervalo()
     console.log ('Numero a adivinar' + arrayNumerosPc)
-    document.getElementById(`numberGuess0`).focus()
+    document.getElementById(`adivinarNumero0`).focus()
 }
 
-function agregarUsuario (usuarioNuevo){
-    usuariosTotales.push (usuarioNuevo)
-    // localStorage.usuariosTotales = JSON.stringify(usuariosTotales) // setItem string
-    let USUARIOSSTRING = JSON.stringify(usuariosTotales)
-    localStorage.setItem('usuarioTotal', USUARIOSSTRING)
+function borrarJuego(){ //Borrar datos de pantalla y variables
+    cantidadDeIntentos = 0
+    arrayNumerosPc = []
+    $("#respuestas").html('')
+    borrarErrores()
+    for (let i = 0; i < cantidadNumeros; i++){ //borro los numeros del input del usuario
+        document.getElementById(`adivinarNumero${i}`).value='' 
+    }
+    $("#intentos").html('')
+    $("#tiempoJuego").html('')
 }
 
-/* Numero a adivinar */
+/* Preparando el juego */
 function generarNumeros() { //Genera x cantidad de numeros que no se repiten entre si
     numeroPC = randomNumber () //Pide por primera vez un numero random
     let posicion=0
@@ -104,29 +125,30 @@ function validarPC(posicionf){ //Se fija que no se repita el numero
     return true // numero validado
 }
 
-/* Numeros Usuario */
-function crearNumberGuess() { //Genero los inputs para que el usuario escriba su numero
+function crearAdivinarNumero() { //Genero los inputs para que el usuario escriba su numero
     let acumulador = ``;
     for (let i = 0; i < cantidadNumeros; i++) {
-        acumulador += `<input id=numberGuess${i} class='numberGuess' type="text" maxlength="1" name="numero"/>`; 
+        acumulador += `<input id=adivinarNumero${i} class='adivinarNumero' type="text" maxlength="1" name="numero"/>`; 
       }
-      /* document.getElementById("numberGuessContainer").innerHTML = acumulador; */ //SIN JQUERY
-      $("#numberGuessContainer").html(`${acumulador}`); //Lo mismo con JQUERY
+      /* document.getElementById("contenedorAdivinarNum").innerHTML = acumulador; */ //SIN JQUERY
+      $("#contenedorAdivinarNum").html(`${acumulador}`); //Lo mismo con JQUERY
 
     // OTRA FORMA DE HACER LO MISMO (lo dejo adrede)
     // for (let i = 0; i < arrayNumerosUsuario.length; i++) {
     //     let input = document.createElement("input")
-    //     document.getElementById("numberGuessContainer").appendChild(input)
-    //     input.setAttribute("id", `numberGuess${i}`);
-    //     input.setAttribute("class", "numberGuess");
+    //     document.getElementById("contenedorAdivinarNum").appendChild(input)
+    //     input.setAttribute("id", `adivinarNumero${i}`);
+    //     input.setAttribute("class", "adivinarNumero");
     //     input.setAttribute("type", "text");
     //     input.setAttribute("maxlength", "1");
     //     input.setAttribute("name", "numero");
     //  }
 }
 
+/* Usuario escribe numero */
+
 function adivinar(){ //Cuando hace click en el boton adivinar
-    document.getElementById(`numberGuess0`).focus()
+    document.getElementById(`adivinarNumero0`).focus()
     borrarErrores()
       if (validarNumeroUsuario()){
           crearArrayNumUsuario()
@@ -134,8 +156,6 @@ function adivinar(){ //Cuando hace click en el boton adivinar
           borrarInput()
       }  
 }
-
-
 
 function borrarErrores(){ //Borra los bordes rojos y los textos
     if (errorVacio != ""){
@@ -151,17 +171,17 @@ function borrarErrores(){ //Borra los bordes rojos y los textos
         errorNoNumero = ""
     }
     for (let i = 0; i < cantidadNumeros; i++){
-    document.getElementById(`numberGuess${i}`).style.border = "none";
+    document.getElementById(`adivinarNumero${i}`).style.border = "none";
     }
 }
 
 function validarNumeroUsuario(){ //Chequeo si completo correctamente los inputs
     let esValido = true
     for (let i = 0; i < cantidadNumeros; i++){
-      let numUsuario=document.getElementById(`numberGuess${i}`).value
+      let numUsuario=document.getElementById(`adivinarNumero${i}`).value
       if (numUsuario == ''|| numUsuario==' '){ //Si algun casillero esta vacio
           esValido = false
-          document.getElementById(`numberGuess${i}`).style.border = "5px solid red";
+          document.getElementById(`adivinarNumero${i}`).style.border = "5px solid red";
           if (errorVacio ==""){
           errorVacio = document.createElement("p")
           errorVacio.innerText = arrayTextoErrores[0]; 
@@ -170,7 +190,7 @@ function validarNumeroUsuario(){ //Chequeo si completo correctamente los inputs
 
       } else if (isNaN (numUsuario)){ //Si alguno de los caracteres no es numerico salta error
           esValido = false
-          document.getElementById(`numberGuess${i}`).style.border = "5px solid red";
+          document.getElementById(`adivinarNumero${i}`).style.border = "5px solid red";
           if (errorNoNumero == ""){            
           errorNoNumero = document.createElement("p")
           errorNoNumero.innerText = arrayTextoErrores[1]; 
@@ -200,10 +220,10 @@ function validarSiRepetido (){ //Chequea si alguno de los numeros se repite
         if (noSeRepite == false){
             return false
         } for (let j=i+1; j < cantidadNumeros; j++){
-            if (document.getElementById(`numberGuess${i}`).value==document.getElementById(`numberGuess${j}`).value){
+            if (document.getElementById(`adivinarNumero${i}`).value==document.getElementById(`adivinarNumero${j}`).value){
             noSeRepite = false;
-            document.getElementById(`numberGuess${i}`).style.border = "5px solid red";
-            document.getElementById(`numberGuess${j}`).style.border = "5px solid red";
+            document.getElementById(`adivinarNumero${i}`).style.border = "5px solid red";
+            document.getElementById(`adivinarNumero${j}`).style.border = "5px solid red";
             return false
             }
         }
@@ -213,9 +233,57 @@ function validarSiRepetido (){ //Chequea si alguno de los numeros se repite
 function crearArrayNumUsuario(){ //Array que guarda el numero dado por el usuario
     arrayNumerosUsuario = []
   for (let i = 0; i < cantidadNumeros; i++){
-      arrayNumerosUsuario.push(parseInt(document.getElementById(`numberGuess${i}`).value))
+      arrayNumerosUsuario.push(parseInt(document.getElementById(`adivinarNumero${i}`).value))
   }
 }
+
+function borrarInput(){ //Borra el valor de los inputs
+    for (let i = 0; i < cantidadNumeros; i++) {
+    document.getElementById(`adivinarNumero${i}`).value='';
+    }
+}
+
+function siguienteInput(){ //Avanza al siguiente input cuando lleno
+    for (let i = 0; i < cantidadNumeros-1 ; i++) {
+        // document.getElementById(`adivinarNumero${i}`).addEventListener("input", function(event){verSiAvanzo(i)});  // SIN JQUERY
+        $(`#adivinarNumero${i}`).on("input", function(event){verSiAvanzo(i)});
+     }
+}
+
+function verSiAvanzo (j){ //Chequea si el input esta lleno para avanzar o no
+    if(document.getElementById(`adivinarNumero${j}`).length = 1){
+        document.getElementById(`adivinarNumero${j+1}`).focus()
+    }
+}
+
+function enterAdivinar() { // si estas en el ultimo input el enter = click Adivinar
+    let enter = document.getElementById(`adivinarNumero${cantidadNumeros-1}`);
+    enter.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) { //el numero 13 es carriage return (enter)
+        event.preventDefault();
+        document.getElementById("adivinar").click();
+      }
+    });
+}
+
+function borrarInputAnterior(){ //con backspace borra y retrocede al input anterior
+    for (let i = 1; i < cantidadNumeros ; i++) {
+        let backspace = document.getElementById(`adivinarNumero${i}`);
+        backspace.addEventListener("keydown", function(event) {
+            if (event.keyCode === 8) { //el numero 8 es backspace
+                event.preventDefault();
+                if (document.getElementById(`adivinarNumero${i}`).value == ""){
+                    document.getElementById(`adivinarNumero${i-1}`).focus()
+                    $(`#adivinarNumero${i-1}`).val("");
+                }  else {
+                    $(`#adivinarNumero${i}`).val(""); //Si el campo del imput esta lleno solo borro sin retroceder
+                }
+            }
+         });
+     }
+}
+
+/* Respuestas */
 
 function comparar(){ //Compara el numero del usuario con el de la PC
     buenas = 0
@@ -232,25 +300,25 @@ function comparar(){ //Compara el numero del usuario con el de la PC
         }
     }
     cantidadDeIntentos += 1
-    crearGuessLine() // Muestra las respuestas
+    crearFilaAdivinada() // Muestra las respuestas
     if (buenas == 4) {
         ganaste()
     } 
 }
 
-function crearGuessLine() { // Muestra las respuestas
-    document.getElementById("respuestas").innerHTML += `<div id=guessLine${cantidadDeIntentos} class="flex guessLine"></div>`; //genera div
+function crearFilaAdivinada() { // Muestra las respuestas
+    document.getElementById("respuestas").innerHTML += `<div id=filaAdivinada${cantidadDeIntentos} class="flex filaAdivinada"></div>`; //genera div
     let acumulador = ``;
     for (let i = 0; i < cantidadNumeros; i++) { //muestra los mismos numeros que pregunto
-        let valor = document.getElementById(`numberGuess${i}`).value
-        acumulador += `<p id=numberGuessOld${cantidadDeIntentos}${i} class='numberGuessOld'>${valor}<p/>`;
+        let valor = document.getElementById(`adivinarNumero${i}`).value
+        acumulador += `<p id=numeroAdivinado${cantidadDeIntentos}${i} class='numeroAdivinado'>${valor}<p/>`;
       }
       acumulador += `<p id=numberAnswer${cantidadDeIntentos} class='numberAnswer'>${buenas} - ${regulares}<p/>` //muestra la respuesta
-      document.getElementById(`guessLine${cantidadDeIntentos}`).innerHTML = acumulador;
+      document.getElementById(`filaAdivinada${cantidadDeIntentos}`).innerHTML = acumulador;
       for (let i = 0; i < cantidadNumeros; i++) {
-        document.getElementById(`numberGuessOld${cantidadDeIntentos}${i}`).style.backgroundImage = "linear-gradient(to top, #ffa0d4, #ff6bce)"; //cambia el color de los ultimos numeros preguntados
+        document.getElementById(`numeroAdivinado${cantidadDeIntentos}${i}`).style.backgroundImage = "linear-gradient(to top, #ffa0d4, #ff6bce)"; //cambia el color de los ultimos numeros preguntados
         if (cantidadDeIntentos!=1){
-            document.getElementById(`numberGuessOld${cantidadDeIntentos-1}${i}`).style.backgroundImage = "linear-gradient(to top, #4b99fd, #4185f7)"; //cambia el color de los numeros preguntados anteriormente
+            document.getElementById(`numeroAdivinado${cantidadDeIntentos-1}${i}`).style.backgroundImage = "linear-gradient(to top, #4b99fd, #4185f7)"; //cambia el color de los numeros preguntados anteriormente
           }
     }
       document.getElementById(`numberAnswer${cantidadDeIntentos}`).style.backgroundImage = "linear-gradient(to top, #ffa0d4, #ff6bce)"; //cambia el color de las ultimas rtas
@@ -259,58 +327,28 @@ function crearGuessLine() { // Muestra las respuestas
       }
 }
       
-function borrarInput(){ //Borra el valor de los inputs
-    for (let i = 0; i < cantidadNumeros; i++) {
-    document.getElementById(`numberGuess${i}`).value='';
-    }
-}
+/* Ganaste */
 
-function borrarJuego(){ //Borrar datos de pantalla y variables
-    cantidadDeIntentos = 0
-    arrayNumerosPc = []
-    $("#respuestas").html('')
-    borrarErrores()
-    for (let i = 0; i < cantidadNumeros; i++){ //borro los numeros del input del usuario
-        document.getElementById(`numberGuess${i}`).value='' 
-    }
-    $("#intentos").html('')
-    $("#tiempoJuego").html('')
-}
-
-function siguienteInput(){ //Avanza al siguiente input cuando lleno
-    for (let i = 0; i < cantidadNumeros-1 ; i++) {
-        // document.getElementById(`numberGuess${i}`).addEventListener("input", function(event){verSiAvanzo(i)});  // SIN JQUERY
-        $(`#numberGuess${i}`).on("input", function(event){verSiAvanzo(i)});
-     }
-}
-
-function verSiAvanzo (j){ //Chequea si el input esta lleno para avanzar o no
-    if(document.getElementById(`numberGuess${j}`).length = 1){
-        document.getElementById(`numberGuess${j+1}`).focus()
-    }
-}
-
-/* GANASTE */
-
-function ganaste(){
+function ganaste(){ // Al ganar muestra info de cual era el nro, en cuantos interntos y tmpo lo adivino, pide el nombre y muestra una frase de curiosidades
     document.getElementById("rta").innerHTML= JSON.stringify(arrayNumerosPc).replace(/,/g, '').replace('[', '').replace(']', '')
     calcularTiempoTranscurrido()
-    animacionGanaste()
+    animacionGanaste() // que aparezca el modal
     mostrarTiempoJuego()
     $("#intentos").html(cantidadDeIntentos)
     document.getElementById("salirGanaste").addEventListener("click", salirGanaste)
-    $('#escribirNombre p').remove();
-    $('#nombreUsuario').css('border-color','white');
+    $('#escribirNombre p').remove(); // borra error de falta nombre
+    $('#nombreUsuario').css('border-color','white'); // borra bordes rojos de error
     document.getElementById("guardar").addEventListener("click", guardar)
 }
 
-function animacionGanaste(){    
-    $("#modalGanaste").show();
-    $(".close").click(function(){
+function animacionGanaste(){    //muestra y oculta el modal ganaste
+    $("#modalGanaste").show(); 
+    $(".close").click(function(){ //cierra el modal en la X pero no resetea el juego
         $("#modalGanaste").hide();
     })
 }
-function mostrarTiempoJuego(){
+
+function mostrarTiempoJuego(){ //tiempo en horas minutos y segundos que le llevo adivinar
  let tiempoJuego =''
  if(hora>1){
     tiempoJuego += `${hora} horas `
@@ -325,12 +363,12 @@ function mostrarTiempoJuego(){
  $("#tiempoJuego").html(tiempoJuego)
 }
 
-function salirGanaste(){ 
+function salirGanaste(){ //oculta el modal al salir y resetea el juego
     $("#modalGanaste").hide();
     resetear()
 }
 
-function guardar(){
+function guardar(){ // si esta completo el input de nombre guarda la info del usaurio
     let nombreUsuario = document.getElementById("nombreUsuario").value
     if (nombreUsuario == ''){
         $('#escribirNombre').append( "<p>*Por favor escribi tu nombre</p>" );
@@ -341,64 +379,27 @@ function guardar(){
     }
 }
 
-function guardando(nombreUsuario){
+function guardando(nombreUsuario){ //info del usuario guardada en el storage y sale
     const usuario1 = new usuario (nombreUsuario,cantidadDeIntentos, fechaDeHoy, segundosTranscurridos);
     agregarUsuario(usuario1)
     salirGanaste()
 }
 
-const URLGET = "../json/curiosidades.json"
+/* Puntaje en storage */
 
-$.get(URLGET, function (respuesta, estado) { 
-    if(estado === "success"){
-      let random =Math.floor(Math.random()*respuesta.length);
-      $("#sabias").html(respuesta[random].curiosidad);
-    }
-})
-    
-// Puntajes
-function getUsuarios() {
+function agregarUsuario (usuarioNuevo){ // Agrega info de puntajes al storage
+    usuariosTotales.push (usuarioNuevo)
+    // localStorage.usuariosTotales = JSON.stringify(usuariosTotales) // setItem string
+    let usuariosString = JSON.stringify(usuariosTotales)
+    localStorage.setItem('usuarioTotal', usuariosString)
+}
+
+function getUsuarios() { // Consigue la info de los puntajes del storage y genera un objeto
     let valorUsuarioEnElStorage = localStorage.getItem('usuarioTotal') //  getItem string
     if (valorUsuarioEnElStorage != null){
         usuariosTotales= JSON.parse(valorUsuarioEnElStorage) // objeto
     } 
 }
-
-//Niveles
-function getNivel(){
-    let nivelString = localStorage.getItem('nivel') //  getItem string
-    if (nivelString != null){
-        nivel= JSON.parse(nivelString)
-    } 
-}
-
-
-// Enter = click Adivinar
-let enter = document.getElementById(`numberGuess${cantidadNumeros-1}`);
-enter.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) { //el numero 13 es carriage return (enter)
-    event.preventDefault();
-    document.getElementById("adivinar").click();
-  }
-});
-// Borrar anterior input
-for (let i = 1; i < cantidadNumeros ; i++) {
-    let backspace = document.getElementById(`numberGuess${i}`);
-    backspace.addEventListener("keydown", function(event) {
-        if (event.keyCode === 8) { //el numero 8 es backspace
-            event.preventDefault();
-            if (document.getElementById(`numberGuess${i}`).value == ""){
-                document.getElementById(`numberGuess${i-1}`).focus()
-                $(`#numberGuess${i-1}`).val("");
-            }  else {
-                $(`#numberGuess${i}`).val(""); //Si el campo del imput esta lleno solo borro sin retroceder
-            }
-        }
-     });
- }
-
-
-
 
 
 //Forma vieja en que validaba datos del input
